4f5d67260d805a6f24df89a0d4fad5d2b39c0cd6
nilton-f-o-junior <nilton.f.o.junior@gmail.com>
2026-01-29 19:54:29 -0300

    feat: continue a escrever o js!

--- src/js/vocabulary.js
+++ src/js/vocabulary.js
@@ -27,7 +27,52 @@ class LanguageLearning {
     this.progress = this.loadProgress();
 
     this.elements = {
-        
+        menuContainer: document.getElementById('menuContainer'),
+        playAudio: document.getElementById('playAudio'),
+        originalText: document.getElementById('originalText'),
+        translationInput: document.getElementById('translationInput'),
+        returnButton: document.getElementById('returnButton'),
+        repeatButton: document.getElementById('repeatButton'),
+        advanceButton: document.getElementById('advanceButton'),
+        playHint: document.getElementById('playHint'),
+        hintText: document.getElementById('hintText'),
     };
+
+    this.init();
   }
+
+
+  async init() {
+    await this.loadLessons();
+    this.setupEventListeners();
+  }
+
+
+  async loadLessons() {
+    try {
+      const response = await fetch('../assets/data/module-01.json');
+      this.lessonsData = await response.json();
+      this.populateMenu();
+
+      const lastLessonId = localStorage.getItem('lastLessonId');
+      if (lastLessonId) {
+        this.loadLesson(lastLessonId);
+      } else if (this.lessonsData.modules.length > 0 &&
+                 this.lessonsData.modules[0].lessons.length > 0) {
+                 
+        this.loadLesson(this.lessonsData.modules[0].lessons[0].id);
+      }
+    } catch (error) {
+      console.error('Erro ao carregar lições:', error);
+      this.showError('Não foi possivel carregar. Verificar arquivo .json!');
+    }
+  }
+
+
+//==========
+//Menu Lateral
+
+
+
+  
 }
